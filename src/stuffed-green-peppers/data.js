b8c731e4261e5044114949815ad439dc5a0b934b
Andrew Miner <aminer@looker.com>
2017-10-11 08:43:40 -0700

    Add recipe for stuffed green peppers

---
+++ src/stuffed-green-peppers/data.js
@@ -0,0 +1,41 @@
+module.exports = require("../helpers")({
+    title: "Stuffed Green Peppers",
+    timings: {
+        prep: "30 min",
+        cook: "30 min",
+    },
+    icons: ["gluten-free"],
+    servings: 6,
+    description: "The filling for these stuffed peppers is a hearty and tasty combination of meat, rice, cheese and " +
+        "vegetable which makes the perfect compliment to the pungent flavor of the pepper itself.",
+    steps: [{
+        ingredients: [
+            { amount: 6, unit: "", item: "green bell peppers" },
+            { unit: "~", item: "salt" },
+        ],
+        directions: "Remove the tops and seeds from the peppers, leaving a hollow shell. Drop these into a pot of " +
+            "boiling water until scalded, drain, sprinkle with salt, and set aside in a baking dish (5 min)."
+    }, {
+        ingredients: [
+            { amount: 1, unit: "lb", item: "lean ground beef" },
+            { amount: 1/3, unit: "cup", item: "onion (diced)" },
+            { amount: 1, unit: "can (14oz)", item: "tomatoes (diced)" },
+            { amount: 1/2, unit: "cup", item: "rice (uncooked)" },
+            { amount: 1/2, unit: "cup", item: "water" },
+            { amount: 1, unit: "tsp", item: "Worcestershire sauce" },
+        ],
+        directions: "In a large skillet, sautée the beef and onions together until the beef is crumbled and brown, " +
+            "and the onions are translucent. Drain any excess grease, and add the tomatoes, rice, water, and " +
+            "Worcestershire sauce. Cover and simmer until the rice is fully cooked and soft. Take care to stir " +
+            "often, and add more water as necessary (about 15 min)."
+    }, {
+        ingredients: [
+            { amount: 1, unit: "cup", item: "chedder cheese (shredded)" },
+            { amount: 2, unit: "cans (12 oz)", item: "tomato soup" },
+        ],
+        directions: "Pre-heat the oven to 350°F. Remove the skillet from the heat and mix in the cheese. Fill the " +
+            "peppers about ¾ full of the mixture. Mix the tomato soup with enough water to create a gravy-like " +
+            "consistency and fill the remaining space in each pepper. Bake until the soup is bubbling and the " +
+            "peppers have just started to singe (about 15-20 min)."
+    }]
+})
