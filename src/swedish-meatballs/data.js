9d253459173281dd90e8ae0b356abfb4c2d3782c
Andrew Miner <andrewminer@mac.com>
2019-03-03 21:32:44 -0700

    Add recipe for Swedish Meatballs

---
+++ src/swedish-meatballs/data.js
@@ -0,0 +1,40 @@
+module.exports = require("../helpers")({
+    title: "Swedish Meatballs",
+    timings: {
+        prep: "60 min",
+    },
+    icons: [],
+    servings: 8,
+    description: "This receipe makes the softest, most succulent meatballs you'll ever try.",
+    steps: [{
+        ingredients: [
+            { amount: 1, unit: "tbsp", item: "butter (unsalted)" },
+            { amount: 1, unit: "tbsp", item: "onion (minced)" },
+        ],
+        directions: "Melt the butter in a skillet and cook the onions until lightly browned."
+    }, {
+        ingredients: [
+            { amount: 1/3, unit: "cup", item: "breadcrumbs" },
+            { amount: 1/2, unit: "cup", item: "water" },
+            { amount: 1/2, unit: "cup", item: "half-n-half" },
+            { amount: 3/4, unit: "lb", item: "ground beef" },
+            { amount: 1/4, unit: "lb", item: "ground pork" },
+            { amount: 2, unit: "tsp", item: "salt" },
+            { amount: 1/3, unit: "tsp", item: "sugar (white)" },
+            { amount: 1/4, unit: "tsp", item: "white pepper (ground)" },
+        ],
+        directions: "In a large bowl, whisk the breadcrumbs, water, and half-n-half together and let sit for a few " +
+            "minutes. Then mix in the remaining ingredients and knead until it forms a smooth paste."
+    }, {
+        ingredients: [
+            { amount: 3, unit: "tbsp", item: "butter (unsalted)" },
+        ],
+        directions: "Melt more butter into the skillet. Using a small spoon, form meatballs and drop them into the " +
+            "melted butter.  Cover and allow to cook on medium heat until an even color all the way through (about " +
+            "10 minutes). Work in batches, setting aside the cooked meatballs in a warm oven to stay hot."
+    }],
+    credit: {
+        name: "Linda T.",
+        url: "https://www.allrecipes.com/recipe/222371/swedish-meatballs-from-a-swede"
+    }
+})
